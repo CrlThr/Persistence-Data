eac89d3901bd61ec6ff66d5a6a3ec420a237cb8d
AmelieHeinrich <amelie.heinrich.dev@gmail.com>
2025-10-10 09:27:49 +0200

    add: csproj

--- src/Game/Program.cs
+++ src/Game/Program.cs
@@ -1,12 +1,12 @@
-using System;
+﻿using System;
 
 namespace Game
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            Console.WriteLine("coucou");
+            Console.WriteLine("Welcome to the Game!");
         }
     }
 }
